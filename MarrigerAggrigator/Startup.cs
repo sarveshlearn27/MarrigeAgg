91ff42dc7dcad443bef4f7a0a61a6485ad36588c
unknown <sarveshlearn27@gmail.com>
2018-11-04 21:05:07 +0530

    Add Files

---
+++ MarrigerAggrigator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarrigerAggrigator.Startup))]
+namespace MarrigerAggrigator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
